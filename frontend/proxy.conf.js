660f5ab41c7fcead9347567ee8adeb3f2b51a5ab
losh11 <loshan1212@gmail.com>
2023-05-22 17:45:16 +0100

    litecoin: update dev proxy

--- frontend/proxy.conf.js
+++ frontend/proxy.conf.js
@@ -1,53 +1,54 @@
-const fs = require('fs');
+const fs = require("fs");
 
 let PROXY_CONFIG;
 let configContent;
 
-const CONFIG_FILE_NAME = 'mempool-frontend-config.json';
+const CONFIG_FILE_NAME = "mempool-frontend-config.json";
 
 try {
-    const rawConfig = fs.readFileSync(CONFIG_FILE_NAME);
-    configContent = JSON.parse(rawConfig);
-    console.log(`${CONFIG_FILE_NAME} file found, using provided config`);
+  const rawConfig = fs.readFileSync(CONFIG_FILE_NAME);
+  configContent = JSON.parse(rawConfig);
+  console.log(`${CONFIG_FILE_NAME} file found, using provided config`);
 } catch (e) {
-    console.log(e);
-    if (e.code !== 'ENOENT') {
-      throw new Error(e);
+  console.log(e);
+  if (e.code !== "ENOENT") {
+    throw new Error(e);
   } else {
-      console.log(`${CONFIG_FILE_NAME} file not found, using default config`);
+    console.log(`${CONFIG_FILE_NAME} file not found, using default config`);
   }
 }
 
 PROXY_CONFIG = [
-    {
-        context: ['*',
-        '/api/**', '!/api/v1/ws',
-        '/testnet/api/**',
-        ],
-        target: "https://mempool.space",
-        ws: true,
-        secure: false,
-        changeOrigin: true
-    },
-    {
-        context: ['/api/v1/ws'],
-        target: "https://mempool.space",
-        ws: true,
-        secure: false,
-        changeOrigin: true,
-    },
-    {
-      context: ['/resources/mining-pools/**'],
-      target: "https://mempool.space",
-      secure: false,
-      changeOrigin: true
-    },
-    {
-        context: ['/resources/assets.json', '/resources/assets.minimal.json', '/resources/worldmap.json'],
-        target: "https://mempool.space",
-        secure: false,
-        changeOrigin: true,
-    }
+  {
+    context: ["*", "/api/**", "!/api/v1/ws", "/testnet/api/**"],
+    target: "https://explorer.litecoin.net",
+    ws: true,
+    secure: false,
+    changeOrigin: true,
+  },
+  {
+    context: ["/api/v1/ws"],
+    target: "https://explorer.litecoin.net",
+    ws: true,
+    secure: false,
+    changeOrigin: true,
+  },
+  {
+    context: ["/resources/mining-pools/**"],
+    target: "https://explorer.litecoin.net",
+    secure: false,
+    changeOrigin: true,
+  },
+  {
+    context: [
+      "/resources/assets.json",
+      "/resources/assets.minimal.json",
+      "/resources/worldmap.json",
+    ],
+    target: "https://explorer.litecoin.net",
+    secure: false,
+    changeOrigin: true,
+  },
 ];
 
 module.exports = PROXY_CONFIG;
--- frontend/proxy.conf.mixed.js
+++ frontend/proxy.conf.mixed.js
@@ -1,42 +1,46 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const FRONTEND_CONFIG_FILE_NAME = 'mempool-frontend-config.json';
+const FRONTEND_CONFIG_FILE_NAME = "mempool-frontend-config.json";
 
 let configContent;
 
-// Read frontend config 
+// Read frontend config
 try {
-    const rawConfig = fs.readFileSync(FRONTEND_CONFIG_FILE_NAME);
-    configContent = JSON.parse(rawConfig);
-    console.log(`${FRONTEND_CONFIG_FILE_NAME} file found, using provided config`);
+  const rawConfig = fs.readFileSync(FRONTEND_CONFIG_FILE_NAME);
+  configContent = JSON.parse(rawConfig);
+  console.log(`${FRONTEND_CONFIG_FILE_NAME} file found, using provided config`);
 } catch (e) {
-    console.log(e);
-    if (e.code !== 'ENOENT') {
-      throw new Error(e);
+  console.log(e);
+  if (e.code !== "ENOENT") {
+    throw new Error(e);
   } else {
-      console.log(`${FRONTEND_CONFIG_FILE_NAME} file not found, using default config`);
+    console.log(
+      `${FRONTEND_CONFIG_FILE_NAME} file not found, using default config`
+    );
   }
 }
 
 let PROXY_CONFIG = [];
 
-PROXY_CONFIG.push(...[
-  {
-    context: ['/api/v1/**'],
-    target: `http://localhost:8999`,
-    secure: false,
-    ws: true,
-    changeOrigin: true,
-    proxyTimeout: 30000,
-  },
-  {
-    context: ['/api/**'],
-    target: `https://mempool.space`,
-    secure: false,
-    changeOrigin: true,
-    proxyTimeout: 30000,
-  }
-]);
+PROXY_CONFIG.push(
+  ...[
+    {
+      context: ["/api/v1/**"],
+      target: `http://localhost:8999`,
+      secure: false,
+      ws: true,
+      changeOrigin: true,
+      proxyTimeout: 30000,
+    },
+    {
+      context: ["/api/**"],
+      target: `https://explorer.litecoin.net`,
+      secure: false,
+      changeOrigin: true,
+      proxyTimeout: 30000,
+    },
+  ]
+);
 
 console.log(PROXY_CONFIG);
 
